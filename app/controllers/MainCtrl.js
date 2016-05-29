00304144a4c8647fd1ebcc5e57460e943ee70eba
Thomas Buida <thomasbuida@gmail.com>
2016-05-29 14:27:05 -0500

    test spotify API call

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -8,6 +8,11 @@ app.controller("MainCtrl", [
 
   function ($scope, $http, $location, firebaseURL) {
     
-
+    $('#search').click(function () {
+      $http.get(`https://api.spotify.com/v1/search?q=tupac&type=artist&limit=1`).then( (response) => {
+        let artist = response.data.artists.items[0];
+        console.log(artist);
+        }, (error) => console.log("error", error))
+      });
   }
 ]);
