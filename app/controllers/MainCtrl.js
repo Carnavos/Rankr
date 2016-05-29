9dbf17a0dcc0a6529b2b88feb496726f7c7ac6ec
Theo Carnavos <tcarnavos@gmail.com>
2016-05-29 18:33:31 -0500

    Added search album by string

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -11,34 +11,58 @@ app.controller("MainCtrl", [
 		
 		// sample 2Pac queries    
     $scope.testArtistString = 'tupac';
-    $scope.tempArtistId = "1ZwdS5xdxEREPySFridCfh";
-    $scope.tempAlbumId = "6IaWlt7CLWVONECmbU4OHp";
+    // $scope.tempArtistId = "1ZwdS5xdxEREPySFridCfh";
+    // $scope.tempAlbumId = "6IaWlt7CLWVONECmbU4OHp";
+    $scope.testAlbumString = 'All Eyez on Me';
+
+
 
 
     // SpotifyFactory getArtistByString test
   	SpotifyFactory.getArtistByString($scope.testArtistString).then(
   		artistObject => {
-  			// should be matching string to string
 				console.log(`artist: `, artistObject);
 				console.log(`artist: `, artistObject.data.artists.items[0].name);
+				// new promise
+				return SpotifyFactory.getArtistAlbumsByArtistId(artistObject.data.artists.items[0].id);
   		},
   		err => console.log(err)
-		);
+		)
 
     // SpotifyFactory getArtistAlbumsByArtistId test
-  	SpotifyFactory.getArtistAlbumsByArtistId($scope.tempArtistId).then(
+  	.then(
   		albumsObject => {
-  			// should be matching string to string
-				console.log(`albums: `, albumsObject);
+				console.log(`albums: `, albumsObject.data.items);
+				// console.log(`first album: `, albumsObject.data.items[0].name);
 				console.log(`first album: `, albumsObject.data.items[0].name);
+				// next promise
+				return SpotifyFactory.getAlbumDetailByAlbumId(albumsObject.data.items[0].id)
+  		},
+  		err => console.log(err)
+		)
+
+    // SpotifyFactory getAlbumDetailByAlbumId test
+  	.then(
+  		albumObject => {
+				console.log(`specific album object: `, albumObject);
+				console.log(`specific album name: `, albumObject.data.name);
+				console.log(`specific album date: `, albumObject.data.release_date);
+				console.log(`specific album date precision: `, albumObject.data.release_date_precision);
   		},
   		err => console.log(err)
 		);
 
-    // SpotifyFactory getAlbumDetailByAlbumId test
-  	SpotifyFactory.getAlbumDetailByAlbumId($scope.tempAlbumId).then(
+
+  	// SpotifyFactory getArtistByString test
+  	SpotifyFactory.getAlbumByString($scope.testAlbumString).then(
   		albumObject => {
-  			// should be matching string to string
+				console.log(`album by string: `, albumObject);
+				return SpotifyFactory.getAlbumDetailByAlbumId(albumsObject.data.items[0].id)
+  		},
+  		err => console.log(err)
+		)
+  	.then(
+  		albumObject => {
 				console.log(`specific album object: `, albumObject);
 				console.log(`specific album name: `, albumObject.data.name);
 				console.log(`specific album date: `, albumObject.data.release_date);
--- app/factories/SpotifyFactory.js
+++ app/factories/SpotifyFactory.js
@@ -11,13 +11,10 @@ app.factory("SpotifyFactory", function ($http, firebaseURL) {
     getArtistByString(artistString) {
       return new Promise((resolve, reject) => {
         console.log(`SpotifyFactory getArtist Test`);
-        let tempArtist = "tupac";
         $http
           // spotify api request url
-          // "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist"
           .get(`https://api.spotify.com/v1/search?q=${artistString}&type=artist&limit=1`)
           .then(
-            // delivers all Firebase upsells
             spotifyObject => resolve(spotifyObject),
             error => reject(error)
           );
@@ -27,13 +24,10 @@ app.factory("SpotifyFactory", function ($http, firebaseURL) {
     getArtistAlbumsByArtistId(artistId) {
       return new Promise((resolve, reject) => {
         console.log(`SpotifyFactory getAlbums Test`);
-        let tempArtistId = "1ZwdS5xdxEREPySFridCfh";
         $http
           // spotify api request url
-          // .get(`https://api.spotify.com/v1/artists/${tempArtistId}/albums`)
-          .get(`https://api.spotify.com/v1/artists/${tempArtistId}/albums`)
+          .get(`https://api.spotify.com/v1/artists/${artistId}/albums`)
           .then(
-            // delivers all Firebase upsells
             spotifyObject => resolve(spotifyObject),
             error => reject(error)
           );
@@ -43,13 +37,24 @@ app.factory("SpotifyFactory", function ($http, firebaseURL) {
     getAlbumDetailByAlbumId(albumId) {
       return new Promise((resolve, reject) => {
         console.log(`SpotifyFactory specfic album Test`);
-        let tempAlbumId = "1ZwdS5xdxEREPySFridCfh";
         $http
           // spotify api request url
           // .get(`https://api.spotify.com/v1/artists/${tempAlbumId}/albums`)
           .get(`https://api.spotify.com/v1/albums/${albumId}`)
           .then(
-          	// delivers all Firebase upsells
+            spotifyObject => resolve(spotifyObject),
+            error => reject(error)
+          );
+      })
+    },
+
+    getAlbumByString(albumString) {
+      return new Promise((resolve, reject) => {
+        console.log(`SpotifyFactory Album Search Test`);
+        $http
+          // spotify api request url
+          .get(`https://api.spotify.com/v1/search?q=${albumString}&type=album&limit=1&album_type=album`)
+          .then(
             spotifyObject => resolve(spotifyObject),
             error => reject(error)
           );
